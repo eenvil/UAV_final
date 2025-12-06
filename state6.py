f250c9ba982b355916bd4969dd9ab1d8e7da340c
hank <zxh.cs12@nycu.edu.tw>
2025-12-06 10:42:00 +0800

    upload yolo folder & fix pyproject

--- state6.py
+++ state6.py
@@ -1,5 +1,5 @@
 # turn and detect the doll to decide next state
 def state6(frame):
-    # todo: implement state1 behavior
+    # todo: implement state6 behavior
     raise NotImplementedError("state6 behavior not implemented yet")
     return [lr, fb, ud, yw], debug_frame, next_state
